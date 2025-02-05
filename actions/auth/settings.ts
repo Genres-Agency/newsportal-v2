c8299e6037906c3de0de36db67e50d1d113a383b
Md Ataullah <ataullahm100@gmail.com>
2025-02-05 23:01:16 +0600

    Fix build error

--- actions/auth/settings.ts
+++ actions/auth/settings.ts
@@ -2,12 +2,9 @@
 import { getUserByEmail, getUserById } from "@/lib/actions/user.action";
 import { currentUser } from "@/lib/auth";
 import { db } from "@/lib/database.connection";
-import { sendVerificationEmail } from "@/lib/mail";
-import { generateVerificationToken } from "@/lib/token";
 import { SettingsSchema } from "@/schema";
 import bcrypt from "bcryptjs";
 import * as z from "zod";
-import { getSession } from "next-auth/react";
 
 export const settings = async (values: z.infer<typeof SettingsSchema>) => {
   const user = await currentUser();
@@ -22,32 +19,13 @@ export const settings = async (values: z.infer<typeof SettingsSchema>) => {
     return { error: "Unauthorized" };
   }
 
-  // if the user is signed in from google or another account
   if (user.isOAuth) {
     values.email = undefined;
     values.password = undefined;
     values.newPassword = undefined;
-    values.isTwoFactorEnabled = undefined;
   }
 
-  //   checking validations for email
-  if (values.email && values.email !== user.email) {
-    const existingUser = await getUserByEmail(values.email);
-
-    if (existingUser && existingUser.id !== user.id) {
-      return { error: "Email already in use!" };
-    }
-
-    const verificationToken = await generateVerificationToken(values.email);
-    await sendVerificationEmail(
-      verificationToken.email,
-      verificationToken.token
-    );
-
-    return { success: "Verification email sent!" };
-  }
-
-  //   checking validations for password
+  // Password change logic
   if (values.password && values.newPassword && dbUser.password) {
     const passwordsMatch = await bcrypt.compare(
       values.password,
@@ -63,16 +41,12 @@ export const settings = async (values: z.infer<typeof SettingsSchema>) => {
     values.newPassword = undefined;
   }
 
-  //   updating the user
   const updatedUser = await db.user.update({
     where: { id: dbUser.id },
     data: {
       ...values,
     },
   });
-
-  //   updating in the session
-  await getSession();
 
   return { success: "Settings Updated!" };
 };
--- schema/index.ts
+++ schema/index.ts
@@ -28,20 +28,6 @@ export const RegisterSchema = z
     path: ["confirmPassword"],
   });
 
-// reset schema
-export const ResetSchema = z.object({
-  email: z.string().email({
-    message: "Email is required",
-  }),
-});
-
-// new password schema
-export const NewPasswordSchema = z.object({
-  password: z.string().min(6, {
-    message: "Minimum of 6 characters required",
-  }),
-});
-
 // settings page schema
 export const SettingsSchema = z.object({
   name: z.optional(z.string()),

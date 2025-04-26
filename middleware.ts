b4ec6cb47850877672aca7097bd76a896defb384
Ataullah <ataullahm100@gmail.com>
2025-04-26 12:46:51 +0600

    Fix secret environment variable in token retrieval for middleware

--- middleware.ts
+++ middleware.ts
@@ -13,7 +13,7 @@ export async function middleware(req: NextRequest) {
 
   const token = await getToken({
     req,
-    secret: process.env.AUTH_SECRET,
+    secret: process.env.NEXTAUTH_SECRET,
     secureCookie: process.env.NODE_ENV === "production",
   });
 
